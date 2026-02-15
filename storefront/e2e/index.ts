55a264ef0e9444f92491085eff5e2ab06933e458
0cheesecoder0 <107916948+0cheesecoder0@users.noreply.github.com>
2026-02-15 16:08:52 +1000

    Add Link component to Home function

--- storefront/e2e/index.ts
+++ storefront/e2e/index.ts
@@ -4,3 +4,14 @@ import { accountFixtures } from "./fixtures/account"
 
 export const test = mergeTests(fixtures, accountFixtures)
 export { expect } from "@playwright/test"
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/TitlePage">Go to Title Typing Page</Link>
+    </div>
+  );
+}
