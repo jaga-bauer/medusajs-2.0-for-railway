2d70b9397ba8b317c9f4a9eba55e821f6f4ee398
jaga-bauer <joel@enfupro.co>
2026-02-15 15:44:08 +1000

    Update index.tsx

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Well done! You have successfully changed things!
+            Well done! You have successfully changed things AGAIN!
           </Heading>
           <Heading
             level="h2"
